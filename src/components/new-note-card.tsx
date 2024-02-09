a034ecf04ced377826f447b80933dc4f172d7c47
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-09 14:52:45 -0300

    feat: created NewNoteCardProps

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,9 +1,13 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { IoChevronBackCircle, IoClose } from 'react-icons/io5'
+import { IoClose } from 'react-icons/io5'
 import { ChangeEvent, FormEvent, useState } from 'react';
 import { toast } from 'sonner'
 
-export function NewNoteCard(){
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void
+}
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps){
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
   const [content, setContent] = useState('');
 
@@ -23,7 +27,10 @@ export function NewNoteCard(){
   function handleSaveNote(event: FormEvent) {
     event.preventDefault()
 
-    console.log(content)
+    onNoteCreated(content)
+
+    setContent('')
+    setShouldShowOnboarding(true)
 
     toast.success('Nota criada com sucesso!')
   }
@@ -66,6 +73,7 @@ export function NewNoteCard(){
                   autoFocus 
                   className='text-sm leading-6 text-slate-400 bg-transparent    resize-none flex-1 outline-none'
                   onChange={handleContentChanged}
+                  value={content}
                 />
                 </>
               )}
