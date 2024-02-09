8dfd60102c0e183ebd34b8eb64c5e02a83b162ba
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-09 14:51:57 -0300

    feat: created notes and function onNoteCreated

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,28 @@
+import { useState } from 'react'
 import logo from './assets/Logo.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+interface Note{
+  id: string
+  date: Date
+  content: string
+}
+
 export default function App() {
+  const [notes, setNotes] = useState<Note[]>([
+    
+  ])
+
+  function onNoteCreated(content: string) {
+    const newNote ={
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    setNotes([newNote, ...notes])
+  }
 
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -10,8 +30,6 @@ export default function App() {
       <form className='w-full'>
         <input 
           type="text" 
-          name="" 
-          id="" 
           placeholder='Busque em suas notas...'
           className='w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none'
         />
@@ -19,15 +37,10 @@ export default function App() {
       <div className='h-px bg-slate-700'/>
 
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
-        <NewNoteCard />
-        <NoteCard note={{
-          date: new Date(),
-          content: 'Hello world',
-        }}/>
-        <NoteCard note={{
-          date: new Date(),
-          content: 'Hello NLW',
-        }}/>
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {notes.map(note => {
+          return <NoteCard key={note.id} note={note}/>
+        })}
       </div>
     </div>
   )
